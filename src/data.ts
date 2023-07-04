d031867db51b362a8f20a24d6d293d0fe3d35c0c
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-07-04 17:07:43 +0200

    it was wrong fix 🥲

--- src/data.ts
+++ src/data.ts
@@ -2,25 +2,25 @@ export const data = [
   {
     category: "Reaction",
     color: "text-light-red",
-    icon: "/icon-reaction.svg",
+    icon: "./icon-reaction.svg",
     score: 80,
   },
   {
     category: "Memory",
     color: "text-orangey-yellow",
-    icon: "/icon-memory.svg",
+    icon: "./icon-memory.svg",
     score: 92,
   },
   {
     category: "Verbal",
     color: "text-green-teal",
-    icon: "/icon-verbal.svg",
+    icon: "./icon-verbal.svg",
     score: 61,
   },
   {
     category: "Visual",
     color: "text-cobalt-blue",
-    icon: "/icon-visual.svg",
+    icon: "./icon-visual.svg",
     score: 72,
   },
 ];
