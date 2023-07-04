3d4ed2a4995f2acd697e0ed33ece0ae7fe6baf2e
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-07-04 19:33:31 +0200

    i'm hopeless...

--- src/data.ts
+++ src/data.ts
@@ -2,25 +2,25 @@ export const data = [
   {
     category: "Reaction",
     color: "text-light-red",
-    icon: "./results-summary-component/icon-reaction.svg",
+    icon: "./icon-reaction.svg",
     score: 80,
   },
   {
     category: "Memory",
     color: "text-orangey-yellow",
-    icon: "./results-summary-component/icon-memory.svg",
+    icon: "./icon-memory.svg",
     score: 92,
   },
   {
     category: "Verbal",
     color: "text-green-teal",
-    icon: "./results-summary-component/icon-verbal.svg",
+    icon: "./icon-verbal.svg",
     score: 61,
   },
   {
     category: "Visual",
     color: "text-cobalt-blue",
-    icon: "./results-summary-component/icon-visual.svg",
+    icon: "./icon-visual.svg",
     score: 72,
   },
 ];
